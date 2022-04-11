cfd6111e266212aee5f7394c9080209dbd963a9f
Kim Diep <kim.diep.techno@gmail.com>
2022-04-11 17:02:12 +0100

    Adds Exercise003 and tests

---
+++ Exercises/Exercise003.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Exercises
+{
+    public class Exercise003
+    {
+        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+
+        public string[] iceCreamFlavours() => throw new NotImplementedException();
+    }
+}
