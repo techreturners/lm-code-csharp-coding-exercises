2135f4acb61a1f31b64e98f5b5d9410284deae66
iamfranco <kinfaifrancochan@gmail.com>
2022-05-03 11:46:03 +0100

    implement exercise001 methods

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -8,32 +8,44 @@ namespace Exercises
     {
         public string CapitalizeWord(string word)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            string firstLetterCapitalized = word[0].ToString().ToUpper();
+            string otherLetters = word.Substring(1);
+            return firstLetterCapitalized + otherLetters;
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return $"{firstName[0]}.{lastName[0]}";
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            double vatComponent = originalPrice * vatRate / 100; // dividing by 100 because vatRate is in percentage
+            double newPrice = originalPrice + vatComponent; 
+            return Math.Round(newPrice, 2); // round newPrice to 2 decimal places
         }
 
         public string Reverse(string sentence)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            string sentenceReversed = "";
+            foreach (char c in sentence)
+            {
+                sentenceReversed = c + sentenceReversed; // prepends new character so the string is built in reverse
+            }
+            return sentenceReversed;
         }
 
         public int CountLinuxUsers(List<User> users)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            int linuxUsersCount = 0;
+            foreach (User user in users)
+            {
+                if (user.Type == "Linux")
+                {
+                    linuxUsersCount++;
+                }
+            }
+            return linuxUsersCount;
         }
     }
 }
