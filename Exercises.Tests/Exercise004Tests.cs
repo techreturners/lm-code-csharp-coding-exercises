117b7426b3042b297a3cd1a29c1355afa12b149b
iamfranco <kinfaifrancochan@gmail.com>
2022-05-03 15:19:17 +0100

    add test for when millisecond is specified

--- Exercises.Tests/Exercise004Tests.cs
+++ Exercises.Tests/Exercise004Tests.cs
@@ -29,5 +29,13 @@ namespace Exercises.Tests
             DateTime expectedResult = new(2052, 10, 3, 1, 46, 39);
             actualResult.Should().Be(expectedResult);
         }
+
+        [Test]
+        public void AddGigaSecond_Should_Return_Result_When_Millisecond_Is_Specified()
+        {
+            DateTime actualResult = Exercise004.AddGigaSecond(new DateTime(2021, 1, 24, 23, 59, 59, 555));
+            DateTime expectedResult = new(2052, 10, 3, 1, 46, 39, 555);
+            actualResult.Should().Be(expectedResult);
+        }
     }
 }
