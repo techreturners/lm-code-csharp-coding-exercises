726e14bd9856a33949562d0cba0b64d78d301bb3
vahidkianfar <vahidkianfar@gmail.com>
2022-05-03 17:51:48 +0100

    Update Exercise004.cs

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -6,8 +6,8 @@ namespace Exercises
         public static DateTime AddGigaSecond(DateTime dateTime)
         {
             // Replace the exception statement below with your code!
-            //throw new NotImplementedException();
-
+            
+            
             TimeSpan gigasec = TimeSpan.FromSeconds(Math.Pow(10, 9));
             return dateTime + gigasec;
 
